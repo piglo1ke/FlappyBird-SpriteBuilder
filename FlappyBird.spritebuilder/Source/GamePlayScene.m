da5768cc0a02cf1d024043bc6abae3e39d20df9a
piglo1ke <kylepiglowski@Kyles-iMac.local>
2014-10-26 02:41:40 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -27,9 +27,7 @@
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
-    [self addObstacle];
-    [self addObstacle];
-    timeSinceObstacle = 0.0f;
+    [self addObstacle];timeSinceObstacle = 0.0f;
 }
 
 @end
